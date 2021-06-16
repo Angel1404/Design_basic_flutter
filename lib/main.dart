9c9249097a2e42b0804c3b2057bda9e26bef2594
Angel Carvajale <angelpalomino1404@gmail.com>
2021-06-15 19:59:27 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:retos_design/app/bindings/home_page_binding.dart';
-import 'package:retos_design/app/ui/theme/linght_theme.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -9,7 +8,6 @@ void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.HOME,
-    //theme: appThemeData1,
     defaultTransition: Transition.fade,
     initialBinding: HomePageBinding(),
     getPages: AppPages.pages,
